9f554551996f7ba87d42d703e7eff0d6724c4606
sakas19 <sarvnaz.kasaei@edu.cmeducations.se>
2023-08-04 08:52:35 +0200

    added weather icons

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState("");
-  const apiKey = "e7136b7cbdd236cf44d2abb469af9dc2"; // Replace with your API key
+  const apiKey = "e7136b7cbdd236cf44d2abb469af9dc2"; 
 
   const fetchWeather = async () => {
     try {
@@ -22,6 +22,27 @@ function App() {
     }
   };
 
+  const getWeatherIcon = (weatherCondition) => {
+    switch (weatherCondition) {
+      case "Clear":
+        return "fas fa-sun"; 
+      case "Clouds":
+        return "fas fa-cloud"; 
+      case "Rain":
+        return "fas fa-cloud-showers-heavy"; 
+      case "Snow":
+        return "fas fa-snowflake"; 
+      case "Thunderstorm":
+        return "fas fa-bolt"; 
+      case "Mist":
+      case "Smoke":
+      case "Haze":
+        return "fas fa-smog"; 
+      default:
+        return "fas fa-question"; 
+    }
+  };
+
   return (
     <div className="weather-app">
       <h1>Weather App</h1>
@@ -34,19 +55,21 @@ function App() {
       <button onClick={fetchWeather}>Get Weather</button>
 
       {weatherData && (
-        <div>
+        <div className="weather-data">
           <h2>{weatherData.name}</h2>
           <p>{weatherData.main.temp}°C</p>
           <p>{weatherData.weather[0].description}</p>
+          <i className={getWeatherIcon(weatherData.weather[0].main)}></i>
         </div>
       )}
 
-      {error && <p>Error: {error}</p>}
+      {error && <p className="error">Error: {error}</p>}
     </div>
   );
 }
 
-export default App; 
+export default App;
+
 
 
 
